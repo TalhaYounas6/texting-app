39db4ba231f7577525fc93ece877627912566f9d
TalhaYounas6 <myounas1168@gmail.com>
2025-09-18 16:52:33 +0500

    Created the signup page and set up useAuthStore hook using Zustand for state management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
-import{Route,Routes} from "react-router-dom"
+import React, { useEffect } from 'react'
+import{Navigate, Route,Routes} from "react-router-dom"
 import NavBar from './components/NavBar'
 import HomePage from "./pages/HomePage"
 import SettingsPage from "./pages/SettingsPage"
 import SignUpPage from "./pages/SignUpPage"
 import ProfilePage from "./pages/ProfilePage"
 import LoginPage from "./pages/LoginPage"
+import { useAuthStore } from './store/useAuthStore'
+import {Loader} from "lucide-react"
+import {Toaster} from "react-hot-toast"
 
 function App() {
+
+  const {authUser,checkAuth,isCheckingAuth} = useAuthStore();
+
+  useEffect(()=>{
+    checkAuth();
+  },[])
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
+  
+
   return (
     <div>
       
       <NavBar/>
 
       <Routes>
-          <Route path='/' element={<HomePage/>} />
-          <Route path='/signup' element={<SignUpPage/>} />
-          <Route path='/login' element={<LoginPage/>} />
+          <Route path='/' element={authUser? <HomePage/> : <Navigate to="/login"/>} />
+          <Route path='/signup' element={!authUser? <SignUpPage/> : <Navigate to="/"/>} />
+          <Route path='/login' element={!authUser? <LoginPage/> : <Navigate to="/" />} />
           <Route path='/settings' element={<SettingsPage/>} />
-          <Route path='/profile' element={<ProfilePage/>} />
+          <Route path='/profile' element={authUser? <ProfilePage/> : "/login"} />
       </Routes>
 
+      <Toaster/>
     </div>
   )
 }
---
+++ client/src/store/useAuthStore.js
@@ -0,0 +1,48 @@
+import {create} from "zustand";
+import {axiosInstance} from "../lib/axios.js"
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set)=>(
+    {
+      isSigningUp: false,
+      isLoggingIn : false,
+      isUpdatingProfile: false,
+      authUser : null,
+      isCheckingAuth : true,
+
+      checkAuth : async()=>{
+        try {
+          const res = await axiosInstance.get("/auth/check");
+          set({authUser : res.data});
+        } catch (error) {
+          console.log("Error checking auth: ",error);
+          set({authUser : null})
+        } finally{
+          set({isCheckingAuth : false})
+        }
+      },
+
+      signup : async(data)=>{
+        set({isSigningUp : true});
+        try {
+          const res = await axiosInstance.post("/auth/signup",data)
+          set({authUser : res.data});
+          toast.success("Account Created Successfully");
+        } catch (error) {
+          toast.error(error.response.data.message)
+        } finally{
+          set({isSigningUp : false});
+        }
+      },
+
+      logout : async()=>{
+        try {
+          await axiosInstance.post("/auth/logout");
+          toast.success("Logged out successfully")
+        } catch (error) {
+          toast.error(error.response.data.message);
+        }
+      },
+
+    }
+))
